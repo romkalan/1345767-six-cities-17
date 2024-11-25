5e1b3a81b5919d92118ce370ecd632a23db026d0
romkalan <romkalan@mail.ru>
2024-11-25 16:47:49 +0300

    add component with main page to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainPage from './pages/main.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+function App() {
+  return (<MainPage />);
+}
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App />
   </React.StrictMode>
 );
